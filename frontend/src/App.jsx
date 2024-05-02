3e5c2d55513d4e26403ebb0ce13df302a5743dfd
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-05-03 01:24:50 +0530

    Update frontend styling and add routing in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
+import { AuthContextProvider } from "./context/AuthContext";
+import { ToastContextProvider } from "./context/ToastContext";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline '>
-        Hello world! Chamikara Vithanage
-      </h1>
-    </>
+    <ToastContextProvider>
+      <AuthContextProvider>
+        <Layout>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Layout>
+      </AuthContextProvider>
+    </ToastContextProvider>
   );
 }
 
