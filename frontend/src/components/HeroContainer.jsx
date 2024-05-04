239b28df7394fec6e3122b1264767494ac1b39df
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-05-04 22:16:12 +0530

    chore: Refactor HeroContainer component for better video handling

--- frontend/src/components/HeroContainer.jsx
+++ frontend/src/components/HeroContainer.jsx
@@ -2,21 +2,19 @@ import PropTypes from "prop-types";
 
 const HeroContainer = ({ children, url }) => {
   return (
-    <div className='relative h-screen w-full'>
-      <div className='absolute inset-0 w-full h-full overflow-hidden'>
-        <video
-          className='absolute inset-0 w-full h-full object-cover'
-          autoPlay
-          loop
-          muted
-        >
-          <source src={url} type='video/mp4' />
-          {/* Add additional source elements for different video formats */}
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    <div className='relative h-screen'>
+      <video
+        className='absolute inset-0 w-full object-cover'
+        autoPlay
+        loop
+        muted
+      >
+        <source src={url} type='video/mp4' />
+        {/* Add additional source elements for different video formats */}
+        Your browser does not support the video tag.
+      </video>
       <div className='absolute inset-0 bg-black opacity-50'></div>
-      <div className='absolute inset-0'>{children}</div>
+      <div className='absolute inset-0 '>{children}</div>
     </div>
   );
 };
