86e689e312fdeac28c6e018830611315fa02a9ad
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-05-04 22:16:37 +0530

    chore: Refactor APOD page component and HeroContainer for improved layout and video handling

--- frontend/src/pages/APOD.jsx
+++ frontend/src/pages/APOD.jsx
@@ -1,4 +1,7 @@
 import { useState, useEffect } from "react";
+import HeroContainer from "../components/HeroContainer";
+import Video from "../videos/APOD.mp4";
+import Loading from "../components/Loading";
 
 function APOD() {
   const [apodData, setApodData] = useState(null);
@@ -15,28 +18,44 @@ function APOD() {
         console.error("Error fetching APOD:", error);
       }
     };
-
     fetchAPOD();
   }, []);
 
   return (
-    <div className='mx-5 mt-4'>
-      {apodData ? (
-        <>
-          <h1 className='text-3xl font-bold'>{apodData.title}</h1>
-          <p className='text-sm text-gray-500 mt-2'>{apodData.date}</p>
-          <img
-            src={apodData.url}
-            alt={apodData.title}
-            className='mt-4 rounded-lg shadow-lg'
-          />
-          <p className='mt-4'>{apodData.explanation}</p>
-        </>
-      ) : (
-        <p>Loading...</p>
-        // <Loading />
-      )}
-    </div>
+    <HeroContainer url={Video} className='overflow-hidden'>
+      <div className='max-w-4x2 mx-auto px-4 sm:px-6 lg:px-8'>
+        {apodData ? (
+          <div className='mt-8'>
+            <div className='flex justify-between items-center mb-8'>
+              <div>
+                <h1 className='text-3xl font-bold text-white'>
+                  {apodData.title}
+                </h1>
+                <p className='text-sm text-gray-500'>{apodData.date}</p>
+              </div>
+            </div>
+            <div className='flex flex-col md:flex-row'>
+              <div className='md:w-1/2 mb-4 md:mb-0'>
+                <img
+                  src={apodData.url}
+                  alt={apodData.title}
+                  className='w-full rounded-lg shadow-lg border-2 border-white'
+                />
+              </div>
+              <div className='md:w-1/2 md:pl-8'>
+                <p className='text-white leading-relaxed'>
+                  {apodData.explanation}
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className='flex justify-center items-center h-screen'>
+            <Loading />
+          </div>
+        )}
+      </div>
+    </HeroContainer>
   );
 }
 
