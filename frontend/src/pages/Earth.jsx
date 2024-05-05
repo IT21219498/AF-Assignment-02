44f56c1392dc388c434f81500edbbcad226a1222
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-05-05 14:29:54 +0530

    feat: Add image download functionality to Earth page

--- frontend/src/pages/Earth.jsx
+++ frontend/src/pages/Earth.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import HeroContainer from "../components/HeroContainer";
 import Video from "../videos/Earth.mp4";
 import Loading from "../components/Loading";
+import ToastContext from "../context/ToastContext";
 
 function Earth() {
   const [date, setDate] = useState("2018-01-01");
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState(null);
+  const { toast } = useContext(ToastContext);
 
+  /**
+   * Fetches the Earth image from NASA API based on the provided coordinates and date.
+   * @returns {Promise<void>} A promise that resolves when the image is fetched and processed.
+   */
   const handleFetchImage = async () => {
+    if (!validateDate(date)) {
+      toast.error(
+        "Please enter a valid date between 2018-01-01 and 2021-06-01"
+      );
+      return;
+    }
     setLoading(true);
     try {
       const response = await fetch(
@@ -22,6 +34,32 @@ function Earth() {
     } finally {
       setLoading(false);
     }
+  };
+
+  const validateDate = (date) => {
+    // check if the date is within the range of 2018-01-01 to 2021-06-01
+    if (date < "2018-01-01" || date > "2021-06-01") {
+      return false;
+    }
+
+    return true;
+  };
+
+  //download image without navigate to the image
+  const handleDownloadImage = async () => {
+    if (!imageUrl) {
+      toast.error("Please fetch an image first!");
+      return;
+    }
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(new Blob([blob]));
+    const link = document.createElement("a");
+    link.href;
+    link.setAttribute("href", url);
+    link.setAttribute("download", "earth.jpg");
+    link.click();
+    window.URL.revokeObjectURL(url);
   };
 
   return (
@@ -57,6 +95,12 @@ function Earth() {
             className='bg-gray-800 border text-white rounded-md px-4 py-2'
           >
             Fetch Image
+          </button>
+          <button
+            className='bg-gray-800 border text-white rounded-md px-4 py-2 ml-2'
+            onClick={handleDownloadImage}
+          >
+            Download Image
           </button>
         </div>
         {loading ? (
